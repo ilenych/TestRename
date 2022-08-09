2b10d560572e38fb3e43b4a2b7cc8e828ba2bcdc
Aleksei Ilenko <34714946+ilenych@users.noreply.github.com>
2022-08-09 12:20:30 +0500

    [asda] lool

--- renaming/ViewController.swift
+++ renaming/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         // some code
-        view.backgroundColor = .red
+        view.backgroundColor = .white
     }
 
 
